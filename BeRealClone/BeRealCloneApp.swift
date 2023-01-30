0120ca626fad549d50ba49e2ac72fe6eae3b5a90
hayd-ios <122998019+hayd-ios@users.noreply.github.com>
2023-01-30 20:25:25 +0000

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by apple on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
